810c086d0de33b5a6dc35d620ca32a8c03f92ffa
joshcoder2000 <jdd1993@hotmail.com>
2024-11-29 21:09:19 -0400

    Add new styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 function App({ toggleLanguage, language }) {
   return (
     <>
-      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#1e3a8a_40%,#0369a1_100%)]"></div>
+      <div className="fixed -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#1e3a8a_40%,#0369a1_100%)]"></div>
       <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
         <NavBar toggleLanguage={toggleLanguage} language={language} />
         <Outlet />
