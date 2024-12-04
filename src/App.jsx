951fc873bc3fcd2b0f008b161ad44447a8b4ec92
joshcoder2000 <jdd1993@hotmail.com>
2024-12-04 18:46:58 -0400

    Adds new styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function App({ toggleLanguage, language }) {
   return (
     <>
-      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#1e3a8a_40%,#0369a1_100%)]"></div>
+      <div className="fixed -z-10 custom-full-h w-full [background:radial-gradient(125%_125%_at_50%_10%,#1e3a8a_40%,#0369a1_100%)]"></div>
       <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
         <NavBar toggleLanguage={toggleLanguage} language={language} />
         <Outlet />
